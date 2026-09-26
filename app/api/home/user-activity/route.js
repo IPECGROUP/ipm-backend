d32661abb281473a31d9fe079994c1edcb52df6b
Aliplayer002 <aligholizade538@gmail.com>
2026-09-26 11:41:55 +0330

    fix

--- app/api/home/user-activity/route.js
+++ app/api/home/user-activity/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
-import { prisma } from "../../../../../lib/prisma";
-import { readSessionActivityLogs } from "../../../../../lib/sessionActivityLog";
+import { prisma } from "../../../../lib/prisma";
+import { readSessionActivityLogs } from "../../../../lib/sessionActivityLog";
 
 export const dynamic = "force-dynamic";
 
